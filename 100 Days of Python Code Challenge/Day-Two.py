96c812668989411887b35d3fce549ec2694b24b7
ebere.okey <okeyebereblessing@gmail.com>
2024-10-04 02:28:40 +0100

    Day one and Day Two challenge completed

--- 100 Days of Python Code Challenge/Day-Two.py
+++ 100 Days of Python Code Challenge/Day-Two.py
@@ -1,14 +1,10 @@
-# 100 Days of Python Challenge - Day 2
-# This challenge is about building a Tip Calculator.
-# The program asks for the total bill, tip amount the user wants to give, and the number of people.
-# It then calculates how much each person should pay, including the tip.
+# 100 Days of Python Challenge - Day 2 Tip Calculator
+# This challenge is about building a Tip Calculator.The program asks for the total bill, tip amount the user wants to give, and the number of people. It then calculates how much each person should pay, including the tip.
 print('Welcome to Tip calculator')
-
 # Get inputs from the user
 total_bill = float(input('What was the total bill?\n $'))
 tip = int(input('How much tip would you like to give? 10, 12 or 15\n'))
 people = int(input('How many people to split the bill?\n'))
-
 # Calculate the tip amount and the total bill and then Split the bill among people
 Bill = (total_bill + tip) / people
 # Display the amount each person should pay
